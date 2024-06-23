8385bf6924c414335167558fe90333ca32df9317
shivaiisc <shivachandhraa@gmail.com>
2024-06-23 05:30:16 +0530

    no comment

--- utils.py
+++ utils.py
@@ -54,8 +54,8 @@ def imgs_to_vid(pre_imgs, out_path, mask=False):
     video = cv2.VideoWriter(out_video_full_path, cv2_fourcc, 60, size) #output video name, fourcc, fps, size
 
     for i in range(len(img)): 
-        im = Image.open(img[i]).convert('L')
-        im = ToTensor()(im).squeeze().unsqueeze(-1)
+        im = Image.open(img[i])#.convert('L')
+        im = ToTensor()(im).permute(1, 2, 0)
         im = im * 255.0
         video.write(im.numpy().astype(np.uint8))
     video.release()
