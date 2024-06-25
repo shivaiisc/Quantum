dc2d3f9c60370e72483444097ba3a28f6c39881a
shivaiisc <shivachandhraa@gmail.com>
2024-06-25 21:47:03 +0530

    no comment

--- visual.py
+++ visual.py
@@ -1,25 +1,39 @@
-from re import U
 import torch 
-from torchvision.transforms import ToTensor 
+from torchvision.transforms import ToTensor, ToPILImage
 from PIL import Image 
+from utils import pth_to_vid
 import os 
+from einops import rearrange
+
+def forward_hook(inst, ip, op): 
+    print('Here') 
+    op = (op-torch.min(op))/(torch.max(op)-torch.min(op)) 
+    op = rearrange(op, '1 c h w -> c h w 1')
+    op = op.repeat(1, 1, 1, 3)
+    print(op.shape, torch.max(op), torch.min(op))
+    pth_to_vid(op.cpu(), os.path.join(args.vis_dir + repr(inst)+ '.mp4'))
+
+
 
 def main(args): 
     if args.model_name == 'q_unet': 
         from models import Q_UNET 
         model = Q_UNET(n_qubits=args.n_qubits).to(args.dev)
+        model.qml_decoder.register_forward_hook(forward_hook)
     elif args.model_name == 'unet': 
         from models import UNET 
         model = UNET().to(args.dev)
+        model.down4.register_forward_hook(forward_hook)
     else:
         raise ValueError('Model Unavailable')
     imgs = os.listdir(args.img_dir)
     for img_file in imgs:
         img_path = os.path.join(args.img_dir, img_file) 
         img = Image.open(img_path).convert('L') 
-        img = ToTensor()(img)
+        img = ToTensor()(img).to(args.dev).unsqueeze(0)
         logits = model(img)
         print(logits.shape)
+        exit()
 
         
 
