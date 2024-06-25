018e7b913173206bf464714c3e0bbcd54cac7aa8
shivaiisc <shivachandhraa@gmail.com>
2024-06-26 01:12:23 +0530

    no comment

--- vis.sh
+++ vis.sh
@@ -6,7 +6,7 @@ python visual.py --model_name q_unet \
 	--model_path ./ckpts/quantum_noise/q_unet-0.25/best_q_unet.pth \
 	--img_dir ./res/images/ --vis_dir ./res/vis/q_unet-0.25/
 
-.python visual.py --model_name q_unet \
+python visual.py --model_name q_unet \
 	--model_path ./ckpts/quantum_noise/q_unet-0.1/best_q_unet.pth \
 	--img_dir ./res/images/ --vis_dir ./res/vis/q_unet-0.1/
 
@@ -14,7 +14,7 @@ python visual.py --model_name unet \
 	--model_path ./ckpts/quantum_noise/unet-0.1/best_unet.pth \
 	--img_dir ./res/images/ --vis_dir ./res/vis/unet-0.1/
 
-.python visual.py --model_name q_unet \
+python visual.py --model_name q_unet \
 	--model_path ./ckpts/quantum_noise/q_unet-0.0/best_q_unet.pth \
 	--img_dir ./res/images/ --vis_dir ./res/vis/q_unet-0.0/
 
