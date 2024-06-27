a228d83005aabb8ef4cf4e44d0e9331d3ccbedd9
shivaiisc <shivachandhraa@gmail.com>
2024-06-28 05:06:48 +0530

    no comment

--- train.py
+++ train.py
@@ -58,7 +58,6 @@ def loop(model, loader, optimizer, criterion, args, mode='train'):
         total_loss_list.append(log_dict['total_loss'])
         # pbar.set_postfix({**metrics, **log_dict})
         pbar.set_postfix(log_dict, refresh=idx%10==0)
-        break
     loss_dct = {f'{mode}_ssim_loss': round(np.mean(ssim_loss_list), 4),
                 f'{mode}_dice_loss': round(np.mean(dice_loss_list), 4),
                 f'{mode}_bce_loss': round(np.mean(bce_loss_list), 4),
