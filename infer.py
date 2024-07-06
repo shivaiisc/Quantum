196dbc9245af453549fa344c87b9d472624515f7
shivaiisc <shivachandhraa@gmail.com>
2024-07-06 09:08:26 +0530

    no comment

--- infer.py
+++ infer.py
@@ -1,127 +1,98 @@
-import csv 
 import torch 
-import numpy as np
-from torchvision.transforms import transforms
 from dataset import Pic_to_Pic_dataset
-from loss import SSIM_DICE_BCE, calc_metrics 
-from utils import load_model, plot
+from models import UNET
 from torch.utils.data import DataLoader
-from tqdm import tqdm 
+from loss import SSIM_DICE_BCE, DiceScore
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd 
+from PIL import Image 
+from torchvision.transforms import ToTensor
 import os 
-torch.set_grad_enabled(False)
+from tqdm import tqdm 
+import cv2
 
-def loop(model, loader, criterion, args): 
-    pbar = tqdm(enumerate(loader)) 
-    pbar.set_description(f'epochs:{args.curr_epoch}/{args.epochs}')
-    ssim_loss_list = list() 
-    dice_loss_list = list() 
-    bce_loss_list = list() 
-    total_loss_list = list()
-    acc_list = list()
-    precision_list = list() 
-    recall_list = list() 
-    f1_score_list = list() 
 
-    for idx, (x, y) in pbar: 
-        x = x.to(args.device)
-        y = y.to(args.device)
-        logits = model(x) 
-        loss = criterion(logits, y)
+def imgs_to_vid(path): 
+    patient_id = path.split('/')[-2]
+    imgs = os.listdir(path)
+    imgs = [os.path.join(path, img) for img in imgs if img.endswith('.png')]
+    imgs = sorted(imgs, key=lambda x: int(x.split('/')[-1].split('.')[0]))
+    img = cv2.imread(imgs[0])
+    os.makedirs('{}/'.format(path), exist_ok=True)
+    video = cv2.VideoWriter('{}/vid.mp4'.format(path), cv2.VideoWriter_fourcc(*'mp4v'), 10, (img.shape[1], img.shape[0]))
+
+    for img_path in tqdm(imgs): 
+        if not img_path.endswith('.png'): 
+            continue
+        img = cv2.imread(img_path)
+        video.write(img)       
+    video.release()
+    os.system('rm ./plots/{}/*.png'.format(patient_id))
+
+
+def infer(model, df):
+    dice_score = DiceScore()
+    patient_ids = np.unique(df.patient_id)
+    assert len(patient_ids) == 1, 'Only one patient_id should be present in the dataframe'
+    patient_id = patient_ids[0]
+    os.makedirs('plots/{}'.format(patient_id), exist_ok=True)
+    for i in tqdm(range(len(df))): 
+        img_path = '/home/shivac/qml-data/' + df.loc[i].img_path
+        mask_path = '/home/shivac/qml-data/' + df.loc[i].mask_path
+        img = Image.open(img_path).convert('L')
+        mask = Image.open(mask_path) 
+        mask = ToTensor()(mask).unsqueeze(0)
+        img = ToTensor()(img).unsqueeze(0)
+        logits = model(img.cuda())
+        dice = round(dice_score(mask.cuda(), logits).item(), 2)
+
+        plt.figure(figsize=(10, 6), facecolor='gray')
+        plt.axis('off')
+        plt.title('Depth: ' + str(i) + ' dice_score: ' + str(dice))
+        plt.subplot(1,3,1)
+        plt.title('img')
+        plt.axis('off')
+        plt.imshow(img[0].permute(1,2,0), cmap='gray')
+        plt.subplot(1,3,2)
+        plt.title('mask')
+        plt.axis('off')
+        plt.imshow(mask[0].permute(1,2,0), cmap='gray')
+        plt.subplot(1,3,3)
+        plt.title('logits')
+        plt.axis('off')
+        plt.imshow(logits[0].detach().cpu().permute(1,2,0), cmap='gray')
+        # plt.tight_layout()
+        plt.savefig('plots/{}/{}.png'.format(patient_id, i))
+        plt.clf() 
+        plt.close()
+
+
+
+def main(args): 
+    df = pd.read_csv('/home/shivac/qml-data/csv_files/val_10_org.csv') 
+    patient_ids = np.unique(df.patient_id)
+    patient_id = np.random.choice(patient_ids, 1)[0]
+    df = df[df.patient_id == patient_id].sort_values('idx')
+    df.reset_index(inplace=True)
+
+    model = UNET().cuda()
+    ckpt = torch.load('./ckpts/quantum_noise/56/best_unet.pth') 
+    model.load_state_dict(ckpt['model_state'])
+    model.eval()
+    print('=====infering on patient_id: {}=====>'.format(patient_id))
+    infer(model, df)
+    print('=====Converting images to video=====>')
+    imgs_to_vid('./plots/{}/'.format(patient_id))
+    print('=====Done=====>')
+    print('=====Video saved at ./plots/{}/vid.mp4=====>'.format(patient_id))
+
         
-        ssim_loss = loss['ssim_loss'] 
-        dice_loss = loss['dice_loss'] 
-        bce_loss = loss['bce_loss'] 
-        loss = args.ssim_ratio * ssim_loss + \
-            args.dice_ratio * dice_loss + \
-            args.bce_ratio * bce_loss 
 
-        metrics = calc_metrics(logits, y)
-        loss_dict = {'dice_loss': round(dice_loss.item(), 4),
-                    'ssim_loss': round(ssim_loss.item(), 4),
-                    'bce_loss': round(bce_loss.item(), 4),
-                    'total_loss': round(loss.item(), 4)}
-        ssim_loss_list.append(loss_dict['ssim_loss'])
-        dice_loss_list.append(loss_dict['dice_loss'])
-        bce_loss_list.append(loss_dict['bce_loss'])
-        total_loss_list.append(loss_dict['total_loss'])
-        acc_list.append(metrics['acc'])
-        precision_list.append(metrics['precision'])
-        recall_list.append(metrics['recall']) 
-        f1_score_list.append(metrics['f1_score']) 
-        pbar.set_postfix({**metrics, **loss_dict}, refresh=idx%10==0)
-    
-    loss_dct = {'ssim_loss': round(np.mean(ssim_loss_list), 4),
-                'dice_loss': round(np.mean(dice_loss_list), 4),
-                'bce_loss': round(np.mean(dice_loss_list), 4),
-                'total_loss': round(np.mean(total_loss_list), 4),
-                'f1_score': round(np.mean(f1_score_list), 4),
-                'precision': round(np.mean(precision_list, 4)),
-                'recall': round(np.mean(recall_list), 4),
-                'acc': round(np.mean(acc_list), 4)}
-    
-    return loss_dct
-        
-def main(args):
-    from torchvision.transforms import ToTensor
-    transform = ToTensor()
-    test_data = Pic_to_Pic_dataset(args.data_csv, transform)
-
-    loader = DataLoader(test_data, args.batch_size) 
-
-    if args.model_name == 'unet':
-        from models import UNET
-        model = UNET(in_ch=args.in_ch, out_ch=args.out_ch).to(args.device)
-    elif args.model_name == 'q_unet': 
-        from models import Q_UNET 
-        model = Q_UNET(in_ch=args.in_ch, out_ch=args.out_ch, n_qubits=args.n_qubits).to(args.device)
-    else:
-        print('model Unavailable')
-        exit()
-
-    args.load_path = f'./ckpts/{args.model_name}.pth'
-    args.load_best_path = f'./ckpts/best_{args.model_name}.pth'
-    model = load_model(model, args, best=args.best)
-    
-    criterion = SSIM_DICE_BCE() 
-    if args.parallel:
-        model = torch.nn.DataParallel(model, device_ids=[0, 1])
-    
-    if not os.path.exists('./res/{}'.format(args.model_name)):
-        os.makedirs('./res/{}'.format(args.model_name))
-    f = open(f'./res/{args.model_name}/{args.model_name}_test.csv', 'w')
-    row = ['dice_loss', 'ssim_loss', 'bce_loss', 'total_loss',
-           'acc', 'precision', 'recall', 'f1_score']
-
-    dct = {k:k for k in row}
-    args.log = csv.DictWriter(f, dct.keys()) 
-    args.log.writerow(dct)
-    print(args)
-    dct = loop(model, loader, criterion, args)
-    args.log.writerow(dct)
-    f.close() 
-
-    
-
-if __name__ == '__main__': 
-    import argparse 
-    parser = argparse.ArgumentParser() 
-    parser.add_argument('-ep', '--epochs', type=int, default=100)
-    parser.add_argument('-m', '--model_name', type=str, default='unet')
-    parser.add_argument('-bs', '--batch_size', type=int, default=16)
-    parser.add_argument('-es', '--early_stop', type=int, default=10) 
-    parser.add_argument('-dp', '--data_csv', type=str, default='../../qml-data/qml_mns.csv') 
-    parser.add_argument('-ic', '--in_ch', type=int, default=1)
-    parser.add_argument('-oc', '--out_ch', type=int, default=1)
-    parser.add_argument('-nq', '--n_qubits', type=int, default=28)
-    parser.add_argument('-sr', '--ssim_ratio', type=float, default=0.33)
-    parser.add_argument('-dr', '--dice_ratio', type=float, default=0.33)
-    parser.add_argument('-br', '--bce_ratio', type=float, default=0.33)
-    parser.add_argument('-p', '--parallel', type=int, default=1)
-    parser.add_argument('-b', '--best', type=int, default=1)
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Infer on the model')
+    parser.add_argument('--model', type=str, default='unet', help='Model to infer on')
+    parser.add_argument('--ckpt', type=str, default='./ckpts/quantum_noise/56/best_unet.pth', help='Path to the model checkpoint')
     args = parser.parse_args()
-
-    args.device = 'cuda'
-    args.early_stopping_idx = 0 
-
     main(args)
-
